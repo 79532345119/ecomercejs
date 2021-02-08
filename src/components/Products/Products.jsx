df597228cb31e25881d11abe02d9a981dd25eecc
Mikhail <your_email@whatever.comlukyanenkomikhail@gmail.com>
2021-02-08 12:20:10 +0300

    create Navbar

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 import Product from "../Products/Product/Product"
+import useStyles from "./styles"
 
 const products = [
     {id: 1, name: "shoes", description: "good shoes", price: "$10", image: "https://sport-marafon.ru/upload/resize_cache/files/iblock/elements/e80e9eb2-9e05-11e2-9e97-e83935202582/f3cd5d99-0de4-11e5-80c8-002590d0f723/210_210_1/f3cd5d990de411e580c8002590d0f723_ba8bfb3b5db111e780c9901b0e95a2a8.jpg"},
@@ -8,8 +9,10 @@ const products = [
 ]
 
 const Products = () => {
+    const classes = useStyles()
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map(product => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
